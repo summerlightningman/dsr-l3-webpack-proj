8bd81f49137a2590893cea50c3c8c359c8a8c4e0
summerlightningman <div8@bk.ru>
2022-03-25 20:05:59 +0300

    add styles for answers

--- src/answer-to-node.ts
+++ src/answer-to-node.ts
@@ -2,6 +2,7 @@ const answerToNode = (text: string) => {
     const elem = document.createElement('li');
     elem.innerText = text;
     elem.classList.add('answer-list__item');
+    elem.classList.add('winx-style-text');
     return elem
 };
 
