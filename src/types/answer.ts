3ac3760ac9d3a8decca372929240f10c88f247cf
summerlightningman <div8@bk.ru>
2022-03-25 19:21:21 +0300

    added interfaces for question and answer

---
+++ src/types/answer.ts
@@ -0,0 +1,7 @@
+export type AnswerText = string;
+export type Mark = string;
+
+export interface Answer {
+    text: AnswerText,
+    mark: Mark
+}
---
+++ src/types/question.ts
@@ -0,0 +1,9 @@
+import {Answer} from "./answer";
+
+export type QuestionTitle = string;
+export type AnswerList = Answer[];
+
+export interface Question {
+    title: QuestionTitle,
+    answerList: AnswerList
+}
