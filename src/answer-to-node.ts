aeef248e0fd7218a9698ac417397318ac7dea707
summerlightningman <div8@bk.ru>
2022-03-25 20:39:14 +0300

    added click event handler for answers

--- src/answer-to-node.ts
+++ src/answer-to-node.ts
@@ -1,6 +1,7 @@
-const answerToNode = (text: string) => {
+const answerToNode = (text: string, onClick: () => void) => {
     const elem = document.createElement('li');
     elem.innerText = text;
+    elem.addEventListener('click', onClick);
     elem.classList.add('answer-list__item');
     elem.classList.add('winx-style-text');
     return elem
--- src/node-functions.ts
+++ src/node-functions.ts
@@ -6,7 +6,9 @@ const $questionTitle: HTMLHeadingElement = document.querySelector('#question');
 const $answerList: HTMLUListElement = document.querySelector('#answer-list');
 
 export const setQuestionText = (text: QuestionTitle) => $questionTitle.innerText = text;
-export const setAnswerList = (answersText: AnswerText[]) => {
+export const setAnswerList = (answersText: AnswerText[], onClick: () => void) => {
     $answerList.innerHTML = '';
-    return answersText.map(answerToNode).forEach(node => $answerList.appendChild(node));
+    return answersText
+        .map(text => answerToNode(text, onClick))
+        .forEach(node => $answerList.appendChild(node));
 };
