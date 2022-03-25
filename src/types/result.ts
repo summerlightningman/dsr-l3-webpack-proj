4831d3d9bb26ea32732f2b31cdc4e24b8e06d831
summerlightningman <div8@bk.ru>
2022-03-25 22:30:00 +0300

    added interface for results

---
+++ src/types/result.ts
@@ -0,0 +1,13 @@
+export type ResultID = string;
+export type ResultTitle = string;
+export type Subtitle = string;
+export type Description = string;
+export type ImgSrc = string;
+
+export interface Result {
+    id: ResultID,
+    title: ResultTitle,
+    subtitle: Subtitle,
+    description: Description,
+    img: ImgSrc
+}
